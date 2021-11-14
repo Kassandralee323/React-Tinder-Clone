70b32ca0c422a56dff3ae299f115afb5c91ccc17
alaeddinedridi <alaeddine.dridi@outlook.com>
2021-11-14 19:16:56 +0100

    tinder is now responsive

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = () => {
 
     return (
         <section className="welcome container-fluid row d-flex align-items-center">
-            <div className="wrapper col-md-12 col-xs-12 text-center">
+            <div className="wrapper col-md-12 col-lg-12 col-xs-12 text-center">
                 <div className="wrapper__title d-flex align-items-start justify-content-center">
                     <h1 className="wrapper__text">Swipe Right</h1>
                     <span className="wrapper__reg">&reg;</span>
@@ -15,9 +15,9 @@ const Welcome = () => {
                     <button className="btn wrapper__create_account">Create Account</button>
                 </div>
                 <div>
-                    <button className="btn wrapper__login d-md-none">Log in</button>
+                    <button className="btn wrapper__login d-lg-none">Log in</button>
                 </div>
-                <div className="img-container d-md-none">
+                <div className="img-container d-lg-none">
                     <img className="wrapper__gplay" src="/g-play.png" alt="" />
                 </div>
                 {noteClass && <div className="wrapper_note">
